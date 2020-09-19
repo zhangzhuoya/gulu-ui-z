12838ff9d1da34a8f44495834e8d4633bb9d4f33
zhangzhuo <1114976368@qq.com>
2020-09-19 22:07:55 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Frank></Frank>
+</template>
+
+<script>
+import Frank from "./components/Frank.vue";
+
+export default {
+  name: "App",
+  components: {
+    Frank,
+  },
+};
+</script>
