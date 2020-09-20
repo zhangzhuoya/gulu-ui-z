dfb6a91247b0c5c870b0836acc1d5ecc5757844e
zhangzhuo <1114976368@qq.com>
2020-09-20 16:13:11 +0800

    初始化路由

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <div>hi</div>
-  <Frank></Frank>
+<div>hi</div>
+<Frank />
+<div>导航栏|
+    <router-link to="/">Frank1</router-link>
+    <router-link to="/xxx">Frank2</router-link>
+</div>
+<router-view></router-view>
 </template>
 
 <script>
 import Frank from "./components/Frank.vue";
 
 export default {
-  name: "App",
-  components: {
-    Frank,
-  },
+    name: "App",
+    components: {
+        Frank,
+    },
 };
 </script>
