de9893720bf54f1403917aaf54118bad095ad1b8
zhangzhuo <1114976368@qq.com>
2020-09-23 18:24:05 +0800

    切换开关桩体

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,9 +1,75 @@
 <template>
-<div>
-    switch组件
-</div>
+<button @click="toggle" :class="{checked:checked}">
+    <span></span>
+
+</button>
 </template>
 
 <script lang="ts">
-export default {}
+import {
+    ref
+} from 'vue'
+export default {
+
+    setup() {
+        const checked = ref(false);
+        const toggle = () => {
+            checked.value = !checked.value;
+        }
+        return {
+            toggle,
+            checked
+        }
+    }
+}
 </script>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+
+button {
+    height: $h;
+    width: $h * 2;
+    border: none;
+    background: #bfbfbf;
+    border-radius: $h/2;
+    position: relative;
+
+    >span {
+        position: absolute;
+        top: 2px;
+        left: 2px;
+        height: $h2;
+        width: $h2;
+        background: white;
+        border-radius: $h2 / 2;
+        transition: all 250ms;
+    }
+
+    &.checked {
+        background: #1890ff;
+
+        >span {
+            left: calc(100% - #{$h2} - 2px);
+        }
+    }
+
+    &:focus {
+        outline: none;
+    }
+
+    &:active {
+        >span {
+            width: $h2 + 4px;
+        }
+    }
+
+    &.checked:active {
+        >span {
+            width: $h2 + 4px;
+            margin-left: -4px;
+        }
+    }
+}
+</style>
